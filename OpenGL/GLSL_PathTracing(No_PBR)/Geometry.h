4dcf26980acaffe1e8f88544ed838f5b2bb005f3
zjypls <61042663+zjypls@users.noreply.github.com>
2022-09-10 15:55:25 +0800

    Add files via upload

--- OpenGL/GLSL_PathTracing(No_PBR)/Geometry.h
+++ OpenGL/GLSL_PathTracing(No_PBR)/Geometry.h
@@ -9,22 +9,33 @@
 //#define METAL 2
 #define LIGHT 2
 #define GLASS 3
-
+#include <cmath>
+#include <iostream>
+#include <algorithm>
 struct vec3 {
     float pos[3];
     vec3():pos{0,0,0}{}
     vec3(float i,float j,float k):pos{i,j,k}{}
     vec3(float i):pos{i,i,i}{}
+    inline float length()const{
+        return sqrtf(pos[0]*pos[0]+pos[1]*pos[1]+pos[2]*pos[2]);
+    }
+    inline vec3 operator/(float a)const{
+        return {x()/a,y()/a,z()/a};
+    }
+    inline vec3 normalize()const{
+        return *this/this->length();
+    }
     inline const float *const ptr() const{
         return pos;
     }
-    inline float x(){
+    inline float x()const{
         return pos[0];
     }
-    inline float y(){
+    inline float y()const{
         return pos[1];
     }
-    inline float z(){
+    inline float z()const{
         return pos[2];
     }
 };
@@ -43,13 +54,34 @@ struct Face{
     vec3 Point;
     vec3 color;
 };
-
+vec3 operator*(const vec3&a ,const vec3& b);
+vec3 operator*(const vec3&a,float b);
+vec3 operator+(const vec3&a,const vec3& b);
+vec3 operator-(const vec3&a,const vec3& b);
 struct Camera{
     vec3 origin;
     vec3 horizon;
     vec3 vertical;
     vec3 leftcorner;
     vec3 color;
+    vec3 ForWord;
+    float theY,theZ,focus,lmd;
+    void Update(double dz,double dy){
+        theY+=dy*lmd;
+        theZ+=dz*lmd;
+        if(theY<0.001)
+            theY=0.001;
+        else if(theY>3.12)
+            theY=3.12;
+        if(theZ< -1.55)
+            theZ=-1.55;
+        else if(theZ>1.55)
+            theZ=1.55;
+        ForWord=vec3{sinf(theY)*sinf(theZ),cos(theY), -sinf(theY)*cosf(theZ)}.normalize();
+        horizon=(ForWord*vec3(0,1,0)).normalize()*horizon.length();
+        vertical=(horizon*ForWord).normalize()*vertical.length();
+        leftcorner=(origin+(ForWord*focus)-horizon*0.5f-vertical*0.5f);
+    }
 };
 
 #endif //GLSL_PATHTRACING_GEOMETRY_H
